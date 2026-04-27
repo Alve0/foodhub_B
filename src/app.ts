b74b3cd83281a9a973e8da92d398d4bf43d7202f
alve <gamingalve@gmail.com>
2026-04-27 18:57:09 +0600

    added better auth

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import dotenv from "dotenv";
 import express, { Application } from "express";
 import cors from "cors";
+import { auth } from "./lib/auth";
+import { toNodeHandler } from "better-auth/node";
+import router from "./routes/routes";
 
 dotenv.config();
 
 const app: Application = express();
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/v1", router);
 
 export default app;
---
+++ src/routes/routes.ts
@@ -0,0 +1,9 @@
+import { Request, Response, Router } from "express";
+
+const router = Router();
+
+router.get("/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello, World!" });
+});
+
+export default router;
